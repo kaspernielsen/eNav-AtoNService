28375975f5861ec4ea08b30655889626978c74de
Nikolaos Vastardis <n.vastardis@gmail.com>
2022-05-13 13:38:29 +0100

    Putting all the model mapping configuration in one place

--- src/main/java/org/grad/eNav/atonService/config/GlobalConfig.java
+++ src/main/java/org/grad/eNav/atonService/config/GlobalConfig.java
@@ -16,10 +16,32 @@
 
 package org.grad.eNav.atonService.config;
 
+import _int.iala_aism.s125.gml._0_0.DataSet;
+import _int.iala_aism.s125.gml._0_0.S125AidsToNavigationType;
+import _int.iho.s100.gml.base._1_0_Ext.CurveProperty;
+import _int.iho.s100.gml.base._1_0_Ext.PointCurveSurfaceProperty;
+import _int.iho.s100.gml.base._1_0_Ext.PointProperty;
+import _int.iho.s100.gml.base._1_0_Ext.SurfaceProperty;
+import lombok.extern.slf4j.Slf4j;
+import org.grad.eNav.atonService.models.domain.s125.AidsToNavigation;
+import org.grad.eNav.atonService.models.domain.s125.S125AtonTypes;
+import org.grad.eNav.atonService.models.domain.s125.S125DatasetInfo;
+import org.grad.eNav.atonService.models.dtos.s125.AidsToNavigationDto;
+import org.grad.eNav.atonService.utils.GeometryS125Converter;
+import org.grad.eNav.atonService.utils.S125DatasetBuilder;
+import org.grad.eNav.s125.utils.S125Utils;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.xml.bind.JAXBException;
+import java.beans.PropertyDescriptor;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * The Global Configuration.
@@ -29,7 +51,14 @@ import org.springframework.context.annotation.Configuration;
  * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
  */
 @Configuration
+@Slf4j
 public class GlobalConfig {
+
+    /**
+     * The Application Operator Name Information.
+     */
+    @Value("${gla.rad.aton-service.info.operatorName:Unknown}")
+    private String appOperatorName;
 
     /**
      * The Model Mapper allows easy mapping between DTOs and domain objects.
@@ -39,7 +68,106 @@ public class GlobalConfig {
     @Bean
     @ConditionalOnMissingBean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        // ================================================================== //
+        // Provide a configuration for all the mapping here to keep things tidy
+        // ================================================================== //
+
+        // Loop all the mapped S-125 AtoN types and configure the model mapper
+        // to translate correctly from the S-125 onto the local classes
+        for(S125AtonTypes type : S125AtonTypes.values()) {
+            modelMapper.createTypeMap(type.getS125Class(), type.getLocalClass())
+                    .implicitMappings()
+                    .addMappings(mapper -> {
+                        mapper.skip(AidsToNavigation::setId); // We don't know if the ID is correct so skip it
+                        mapper.using(ctx -> new GeometryS125Converter().convertToGeometry(((S125AidsToNavigationType) ctx.getSource())))
+                                .map(src-> src, AidsToNavigation::setGeometry);
+                    });
+        }
+
+        // Create the Base Aids to Navigation type map
+        modelMapper.createTypeMap(AidsToNavigation.class, AidsToNavigationDto.class)
+                .addMappings(mapper -> {
+                    mapper.using(ctx -> S125AtonTypes.fromLocalClass(((AidsToNavigation) ctx.getSource()).getClass()).getDescription())
+                            .map(src -> src, AidsToNavigationDto::setAtonType);
+                    mapper.using(ctx -> this.convertTos125DataSet(modelMapper, Collections.singletonList((AidsToNavigation) ctx.getSource())))
+                            .map(src -> src, AidsToNavigationDto::setContent);
+                });
+
+        // Add the base to all Aids to Navigation Mappings
+        for(S125AtonTypes atonType : S125AtonTypes.values()) {
+            // Skip the unknown type, we don't need it
+            if(atonType == S125AtonTypes.UNKNOWN) {
+                continue;
+            }
+            modelMapper.createTypeMap(atonType.getLocalClass(), AidsToNavigationDto.class)
+                    .includeBase(AidsToNavigation.class, AidsToNavigationDto.class);
+            modelMapper.createTypeMap(atonType.getLocalClass(), atonType.getS125Class())
+                    .addMappings(mapper -> {
+                        mapper.map(AidsToNavigation::getId, S125AidsToNavigationType::setId);
+                        mapper.using(ctx -> this.convertToS125Geometry((AidsToNavigation) ctx.getSource()))
+                                .map(src -> src, (dest, val) -> {
+                                    try {
+                                        new PropertyDescriptor("geometry", atonType.getS125Class()).getWriteMethod().invoke(dest, val);
+                                    } catch (Exception ex) {
+                                        this.log.error(ex.getMessage());
+                                    }
+                                });
+                    });
+        }
+
+        // ================================================================== //
+
+        return modelMapper;
+    }
+
+    /**
+     * Converts a whole list of Aids to Navigation objects into an XML string
+     * representation conforming to the S-125 data product specification.
+     *
+     * @param atons the list of the Aids to Navigation objects
+     * @return the respective S-125 data string representation
+     */
+    protected String convertTos125DataSet(ModelMapper modelMapper, List<AidsToNavigation> atons) {
+        final S125DatasetBuilder s125DatasetBuilder = new S125DatasetBuilder(modelMapper);
+        final S125DatasetInfo datasetInfo = new S125DatasetInfo(
+                "AtoN Dataset for " + atons.stream().map(AidsToNavigation::getAtonNumber).collect(Collectors.joining(" ")),
+                appOperatorName.replaceAll(" ","_"),
+                atons
+        );
+        final DataSet dataset = s125DatasetBuilder.packageToDataset(datasetInfo, atons);
+        try {
+            return S125Utils.marshalS125(dataset);
+        } catch (JAXBException ex) {
+            this.log.error(ex.getMessage());
+        }
+        return "";
+    }
+
+    /**
+     * Converts the geometry for an Aids to Navigation object for the local JTS
+     * format used for persistence to the S-125 data product specification
+     * compatible one.
+     *
+     * @param aidsToNavigation the Aids to Navigation to get the geometry from
+     * @return the S-125 compliant geometry description
+     */
+    protected Object convertToS125Geometry(AidsToNavigation aidsToNavigation) {
+        PointCurveSurfaceProperty pointCurveSurfaceProperty = new GeometryS125Converter().convertFromGeometry(aidsToNavigation);
+        if(pointCurveSurfaceProperty.getSurfaceProperty() != null) {
+            SurfaceProperty surfaceProperty = new SurfaceProperty();
+            surfaceProperty.setSurfaceProperty(pointCurveSurfaceProperty.getSurfaceProperty());
+            return surfaceProperty;
+        } else if(pointCurveSurfaceProperty.getSurfaceProperty() != null) {
+            CurveProperty curveProperty = new CurveProperty();
+            curveProperty.setCurveProperty(pointCurveSurfaceProperty.getCurveProperty());
+            return curveProperty;
+        } else {
+            PointProperty pointProperty = new PointProperty();
+            pointProperty.setPointProperty(pointCurveSurfaceProperty.getPointProperty());
+            return pointProperty;
+        }
     }
 
 }
