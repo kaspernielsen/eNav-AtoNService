ab29ec974239d3189da00e644186cc68dfcda026
Nikolaos Vastardis <n.vastardis@gmail.com>
2022-10-13 13:59:07 +0100

    Adding the missing certificate ID to the alias

--- src/main/java/org/grad/eNav/atonService/components/SecomCertificateProviderImpl.java
+++ src/main/java/org/grad/eNav/atonService/components/SecomCertificateProviderImpl.java
@@ -70,10 +70,11 @@ public class SecomCertificateProviderImpl implements SecomCertificateProvider {
         final DigitalSignatureCertificate digitalSignatureCertificate = new DigitalSignatureCertificate();
 
         // Get the signature certificate from cKeeper
-        final SignatureCertificateDto response = this.cKeeperClient.getSignatureCertificate(this.appName, null, McpEntityType.SERVICE.getValue());
+        final SignatureCertificateDto response = this.cKeeperClient.getSignatureCertificate("aton-service", null, McpEntityType.SERVICE.getValue());
 
         // Build the SECOM digital certificate object
         try {
+            digitalSignatureCertificate.setCertificateAlias(String.format("%d", response.getCertificateId()));
             digitalSignatureCertificate.setCertificate(SecomPemUtils.getCertFromPem(response.getCertificate()));
             digitalSignatureCertificate.setPublicKey(digitalSignatureCertificate.getCertificate().getPublicKey());
             digitalSignatureCertificate.setRootCertificate(SecomPemUtils.getCertFromPem(response.getRootCertificate()));
