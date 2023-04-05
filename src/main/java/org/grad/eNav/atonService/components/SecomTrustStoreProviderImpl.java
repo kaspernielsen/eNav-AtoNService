47f41873007818f37023cf46677a56312daadbbd
Nikolaos Vastardis <Nikolaos.Vastardis@gla-rad.org>
2023-04-05 13:43:07 +0100

    Fixing the SECOM truststore provider

--- src/main/java/org/grad/eNav/atonService/components/SecomTrustStoreProviderImpl.java
+++ src/main/java/org/grad/eNav/atonService/components/SecomTrustStoreProviderImpl.java
@@ -19,6 +19,8 @@ package org.grad.eNav.atonService.components;
 import lombok.extern.slf4j.Slf4j;
 import org.grad.secom.core.base.SecomTrustStoreProvider;
 import org.grad.secom.core.utils.KeyStoreUtils;
+import org.grad.secom.springboot3.components.SecomConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -42,28 +44,16 @@ import java.security.cert.CertificateException;
 public class SecomTrustStoreProviderImpl implements SecomTrustStoreProvider {
 
     /**
-     * The X.509 Trust-Store.
-     */
-    @Value("${gla.rad.aton-service-client.secom.trustStore:truststore.jks}")
-    String trustStore;
-
-    /**
-     * The X.509 Trust-Store Password.
-     */
-    @Value("${gla.rad.aton-service-client.secom.trustStorePassword:password}")
-    String trustStorePassword;
-
-    /**
-     * The X.509 Trust-Store Type.
-     */
-    @Value("${gla.rad.aton-service-client.secom.trustStoreType:JKS}")
-    String trustStoreType;
-
-    /**
      * The X.509 Root Certificate Alias.
      */
-    @Value("${gla.rad.aton-service-client.secom.rootCertificateAlias:rootCertificate}")
+    @Value("${gla.rad.aton-service.secom.rootCertificateAlias:rootCertificate}")
     String rootCertificateAlias;
+
+    /**
+     * The SECOM Configuration properties.
+     */
+    @Autowired
+    SecomConfigProperties secomConfigProperties;
 
     /**
      * Returns the alias of the root certificate as it is contained in the
@@ -78,7 +68,7 @@ public class SecomTrustStoreProviderImpl implements SecomTrustStoreProvider {
     }
 
     /**
-     * Returns the trust store that contains the trusted SECOm certificate chain.
+     * Returns the trust store that contains the trusted SECOM certificate chain.
      * This is required to validate the received certificate for every applicable
      * request.
      *
@@ -87,7 +77,7 @@ public class SecomTrustStoreProviderImpl implements SecomTrustStoreProvider {
     @Override
     public KeyStore getTrustStore() {
         try {
-            return KeyStoreUtils.getKeyStore(this.trustStore, this.trustStorePassword, this.trustStoreType);
+            return KeyStoreUtils.getKeyStore(this.secomConfigProperties.getTruststore(), this.secomConfigProperties.getTruststorePassword(), this.secomConfigProperties.getTruststoreType());
         } catch (KeyStoreException | NoSuchAlgorithmException | IOException | CertificateException ex) {
             return null;
         }
