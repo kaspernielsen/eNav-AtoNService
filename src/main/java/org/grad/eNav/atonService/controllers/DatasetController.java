de4a8c2f6e75ed9d07551702cb04cdc72f71ec23
Nikolaos Vastardis <n.vastardis@gmail.com>
2023-09-11 17:16:33 +0100

    Adding a transaction to the dataset controller create function

--- src/main/java/org/grad/eNav/atonService/controllers/DatasetController.java
+++ src/main/java/org/grad/eNav/atonService/controllers/DatasetController.java
@@ -32,6 +32,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -115,6 +116,7 @@ public class DatasetController {
      * @param dataSetDto the dataset to create
      * @return the ResponseEntity with status 201 (Created) and with body the new instance, or with status 400 (Bad Request) if the instance has already an ID
      */
+    @Transactional // <- We need this to read the update content for the first time
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<S125DataSetDto> createDataset(@RequestBody S125DataSetDto dataSetDto) {
         log.debug("REST request to save Dataset : {}", dataSetDto);
